489ca5bbfb176a63019c04f03aff6a25756aa08a
NASHRY AKBAR <nashryakbar90@gmail.com>
2025-09-26 23:14:47 +0800

    Add files via upload

---
+++ C/PRAK002-2510817210014-NashryAkbar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Selamat Pagi, Nashry Akbar\n");
+    printf("Selamat Siang, Nashry Akbar\n");
+    printf("Selamat Malam,  Nashry Akbar\n");
+
+    return 0;
+}
---
+++ C/PRAK004-2510817210014-NashryAkbar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    printf("##############################\n");
+    printf("#                            #\n");
+    printf("#        Nashry Akbar        #\n");
+    printf("#       2510817210014        #\n");
+    printf("#                            #\n");
+    printf("##############################");
+
+    return 0;
+}
